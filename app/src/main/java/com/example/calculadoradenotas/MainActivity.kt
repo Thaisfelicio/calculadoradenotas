9c670fe756c95870e8b76afcbc15512a6fd15a43
ThaisFelicio <thsfelicio@gmail.com>
2021-08-25 17:32:59 -0300

    implementando funções ao botões

--- app/src/main/java/com/example/calculadoradenotas/MainActivity.kt
+++ app/src/main/java/com/example/calculadoradenotas/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.calculadoradenotas
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btCalcular: Button = calcular
+        val resultado: TextView = resultado
+        btCalcular.setOnClickListener {
+            val nota1: Int = Integer.parseInt( nota1.text.toString())
+            val nota2: Int = Integer.parseInt( nota2.text.toString())
+            val media: Int = (nota1 + nota2)/2
+            val faltas: Int = Integer.parseInt(faltas.text.toString())
+            if (media >=6 && faltas <=10) {
+                resultado.setText("Aluno foi Aprovado" + "\n" + "Nota Final:" + media + "\n" + "Faltas:" + faltas)
+            }else {
+                resultado.setText("Aluno foi Reprovado"+"\n"+"Nota Final:"+media+"\n"+"Faltas:"+faltas)
+            }
+        }
     }
 }
